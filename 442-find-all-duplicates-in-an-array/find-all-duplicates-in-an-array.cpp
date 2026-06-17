d37c4747cfbfe4bec68de25735bb9d05877d34fa
hiteshshee <hiteshshee12@gmail.com>
2026-06-17 17:17:39 +0530

    Time: 4 ms (64.30%) | Memory: 48.5 MB (62.05%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,16 +1,37 @@
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+//         vector<int> ans;
+//         unordered_map<int,int> freq;
+//         for(int i=0;i<nums.size();i++){
+//             freq[nums[i]]++;
+//         }
+//         for(auto it=freq.begin();it!=freq.end();it++){
+//             if(it->second==2){
+//                 ans.push_back(it->first);
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+
         vector<int> ans;
-        unordered_map<int,int> freq;
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
-        }
-        for(auto it=freq.begin();it!=freq.end();it++){
-            if(it->second==2){
-                ans.push_back(it->first);
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int idx = abs(nums[i]) - 1;
+
+            if(nums[idx] < 0) {
+                ans.push_back(abs(nums[i]));
+            }
+            else {
+                nums[idx] *= -1;
             }
         }
+
         return ans;
     }
 };
