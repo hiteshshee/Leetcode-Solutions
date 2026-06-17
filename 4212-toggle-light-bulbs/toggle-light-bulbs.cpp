b96790dfb5390d760b6729978fe88467321adb7e
hiteshshee <hiteshshee12@gmail.com>
2026-06-17 11:59:04 +0530

    Time: 1 ms (64.98%) | Memory: 34.7 MB (32.08%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
@@ -1,18 +1,36 @@
+// class Solution {
+// public:
+//     vector<int> toggleLightBulbs(vector<int>& bulbs) {
+//         unordered_map<int,int> freq;
+//         set<int> ans;
+//         int n=bulbs.size();
+//         for(int i=0;i<n;i++){
+//             freq[bulbs[i]]++;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(freq[bulbs[i]]%2==1){
+//                 ans.insert(bulbs[i]);
+//             }
+//         }
+//         vector<int> result(ans.begin(), ans.end());
+//        return result;
+//     }
+// };
 class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
         unordered_map<int,int> freq;
-        set<int> ans;
+        vector<int> ans;
         int n=bulbs.size();
         for(int i=0;i<n;i++){
             freq[bulbs[i]]++;
         }
-        for(int i=0;i<n;i++){
-            if(freq[bulbs[i]]%2==1){
-                ans.insert(bulbs[i]);
+        for(auto it=freq.begin();it!=freq.end();it++){
+            if((it->second)%2==1){
+                ans.push_back(it->first);
             }
         }
-        vector<int> result(ans.begin(), ans.end());
-       return result;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
